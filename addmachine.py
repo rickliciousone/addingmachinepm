3904e8290394001c1d41d91d9aa55cd9e0d544f4
Josh Rychlicki <37146312+rickliciousone@users.noreply.github.com>
2022-04-05 11:44:16 -0400

    Delete addmachine.py

--- addmachine.py
+++
@@ -1,4 +0,0 @@
-first_number = int(input("What is the first number you want to add?"))
-second_number = int(input("What is the second number you want to add?"))
-sum = first_number + second_number
-print("The sum of your two numbers is", sum) 
